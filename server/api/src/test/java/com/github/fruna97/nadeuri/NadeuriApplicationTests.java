c7c1bb11069ff6a15b8ee1705e7ee3069848683e
Fruna97 <kdlrlgusk@gmail.com>
2025-02-22 05:30:59 +0900

    first commit

---
+++ server/api/src/test/java/com/github/fruna97/nadeuri/NadeuriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.fruna97.nadeuri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NadeuriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
