dd303cce7a267e5dc7ed3e0c323a870453fd6492
Fruna97 <kdlrlgusk@gmail.com>
2025-02-25 19:48:17 +0900

    feat(api/member): MemoryMemberRepository 단위 테스트 추가  - 이메일로 Member 검색 테스트

--- server/api/src/test/java/com/github/fruna97/nadeuri/repository/MemoryMemberRepositoryTest.java
+++ server/api/src/test/java/com/github/fruna97/nadeuri/repository/MemoryMemberRepositoryTest.java
@@ -1,12 +1,14 @@
 package com.github.fruna97.nadeuri.repository;
 
-import org.assertj.core.api.Assertions;
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
 
 import com.github.fruna97.nadeuri.domain.Member;
 
 class MemoryMemberRepositoryTest {
-    MemoryMemberRepository repository = new MemoryMemberRepository();
+
+    MemoryMemberRepository memberRepository = new MemoryMemberRepository();
     
     @Test
     void save() {
@@ -15,10 +17,24 @@ class MemoryMemberRepositoryTest {
         member.setPassword("test_password");
         member.setUsername("test_username");
         
-        repository.save(member);
+        memberRepository.save(member);
 
-        Member result = repository.findById(member.getId()).get();
+        Member result = memberRepository.findById(member.getId()).get();
 
-        Assertions.assertThat(result).isEqualTo(member);
+        assertThat(result).isEqualTo(member);
+    }
+
+    @Test
+    void findByEmail() {
+        Member member1 = new Member();
+        member1.setEmail("test_email_1@test.com");
+        memberRepository.save(member1);
+        
+        Member member2 = new Member();
+        member2.setEmail("test_email_2@test.com");
+        memberRepository.save(member2);
+        
+        Member result = memberRepository.findByEmail("test_email_2@test.com").get();
+        assertThat(result).isEqualTo(member2);
     }
 }
