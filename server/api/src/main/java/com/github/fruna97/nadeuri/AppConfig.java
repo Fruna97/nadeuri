7a25ec26603083aed7ed2387983f65e0d3b1660a
Fruna97 <kdlrlgusk@gmail.com>
2025-02-26 07:06:44 +0900

    fix(api/member): 등록하지 않은 빈 등록

--- server/api/src/main/java/com/github/fruna97/nadeuri/AppConfig.java
+++ server/api/src/main/java/com/github/fruna97/nadeuri/AppConfig.java
@@ -2,15 +2,21 @@ package com.github.fruna97.nadeuri;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import com.github.fruna97.nadeuri.repository.MemberRepository;
 import com.github.fruna97.nadeuri.repository.MemoryMemberRepository;
 
 @Configuration
 public class AppConfig {
-    
+
     @Bean
     public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bcCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
