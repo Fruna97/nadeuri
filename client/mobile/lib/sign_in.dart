5c931f9987dff70462ee9d6d8360b1cc82324041
Fruna97 <kdlrlgusk@gmail.com>
2025-02-28 21:12:16 +0900

    feat(mobile/auth): 로그인 UI 추가  - 이메일, 비밀번호 입력 필드 추가  - 비밀번호 찾기 버튼 추가  - 로그인 버튼 추가  - OAuth 인증 버튼 추가  - 회원가입 버튼 추가

---
+++ client/mobile/lib/sign_in.dart
@@ -0,0 +1,136 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+
+class SignInPage extends StatelessWidget {
+  const SignInPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: ListView(
+            shrinkWrap: true,
+            physics: ClampingScrollPhysics(),
+            children: <Widget>[
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 12.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    labelText: "이메일",
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 8.0),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 12.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    labelText: "비밀번호",
+                    border: OutlineInputBorder(),
+                  ),
+                  obscureText: true,
+                ),
+              ),
+              const SizedBox(height: 12.0),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    TextButton(
+                      onPressed: () {
+                        log("비밀번호 찾기 button pressed");
+                      },
+                      style: TextButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(4.0),
+                        ),
+                        tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                        padding: const EdgeInsets.all(0),
+                        minimumSize: Size.zero,
+                      ),
+                      child: const Text("비밀번호 찾기"),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 12.0),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                child: ElevatedButton(
+                  onPressed: () {
+                    log("로그인 button pressed");
+                  },
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(4.0),
+                    ),
+                  ),
+                  child: const Text("로그인"),
+                ),
+              ),
+              const SizedBox(height: 18.0),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 12.0),
+                child: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    Expanded(child: Divider(endIndent: 12.0)),
+                    Text("또는"),
+                    Expanded(child: Divider(indent: 12.0)),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 18.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      log("카카오로 로그인 button pressed");
+                    },
+                    style: ElevatedButton.styleFrom(shape: CircleBorder()),
+                    child: Text("K"), // TODO: 카카오 OAuth 로고로 교체
+                  ),
+                ],
+              ),
+              const SizedBox(height: 18.0),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 12.0),
+                child: Divider(),
+              ),
+              const SizedBox(height: 18.0),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    const Text("아직 회원이 아니신가요?"),
+                    const SizedBox(width: 12.0),
+                    TextButton(
+                      onPressed: () {
+                        log("회원가입 button pressed");
+                      },
+                      style: TextButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(4.0),
+                        ),
+                        tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                        padding: const EdgeInsets.all(0.0),
+                        minimumSize: Size.zero,
+                      ),
+                      child: const Text("회원가입 하기"),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
