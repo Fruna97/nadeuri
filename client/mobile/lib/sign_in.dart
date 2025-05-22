2a6db13044dd5f096e987d2a7116877a4b9c1337
Fruna97 <kdlrlgusk@gmail.com>
2025-05-23 03:52:49 +0900

    feat(mobile/auth): 로그인 요청 로직 추가  - 입력 상태, 응답, 내부 오류에 따른 검증 메시지 추가

--- client/mobile/lib/sign_in.dart
+++ client/mobile/lib/sign_in.dart
@@ -1,6 +1,11 @@
+import 'dart:convert';
 import 'dart:developer';
+import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+import 'dto/response_dto.dart';
 
 class SignInPage extends StatefulWidget {
   const SignInPage({super.key});
@@ -66,7 +71,7 @@ class _SignInPageState extends State<SignInPage> {
                 if (_validationMessage.isNotEmpty) Center(child: Text(_validationMessage, style: TextStyle(color: Colors.red, fontSize: 12))),
                 const SizedBox(height: 12.0),
                 ElevatedButton(
-                  onPressed: () {
+                  onPressed: () async {
                     log("로그인 button pressed");
                     final String email = _emailController.text;
                     final String password = _passwordController.text;
@@ -80,7 +85,58 @@ class _SignInPageState extends State<SignInPage> {
                         _validationMessage = "비밀번호를 입력해 주세요.";
                       });
                     } else {
+                      showDialog(context: context, builder: (context) => const Center(child: CircularProgressIndicator()));
+
+                      try {
+                        final response = await http
+                            .post(
+                              // Uri.parse("http://localhost:8080/member/signin"), // Chrome
+                              Uri.parse("http://10.0.2.2:8080/member/signin"), // Android
+                              headers: {"content-type": "application/json; charset=UTF-8"},
+                              body: jsonEncode({"email": email, "password": password}),
+                            )
+                            .timeout(
+                              const Duration(seconds: 5),
+                              onTimeout: () {
+                                return http.Response(
+                                  jsonEncode({"message": "타임아웃", "data": null}),
+                                  HttpStatus.requestTimeout,
+                                  headers: {"content-type": "application/json;charset=UTF-8"},
+                                );
+                              },
+                            );
+
+                        final statusCode = response.statusCode;
+                        log("StatusCode : ${statusCode.toString()}");
+
+                        if (statusCode != HttpStatus.ok) {
+                          setState(() {
+                            _validationMessage = "이메일 또는 비밀번호가 잘못 되었습니다.\n아이디와 비밀번호를 정확히 입력해 주세요.";
+                          });
+                        } else {
+                          try {
+                            final body = jsonDecode(response.body);
+                            final responseDto = ResponseDto.fromJson(body, ResponseDto.dataFromFieldValidation);
+                            log(responseDto.toString());
+
                             
+                          } catch (e) {
+                            log(e.toString());
+                            setState(() {
+                              _validationMessage = "문제가 발생했습니다. 잠시 후 다시 시도해 주세요.";
+                            });
+                          }
+                        }
+                      } catch (e) {
+                        setState(() {
+                          _validationMessage = "문제가 발생했습니다. 문제가 반복된다면, 고객센터에 문의해주세요.";
+                        });
+                      }
+
+                      if (context.mounted) {
+                        Navigator.pop(context);
+                      }
+
                     }
                   },
                   style: ElevatedButton.styleFrom(
