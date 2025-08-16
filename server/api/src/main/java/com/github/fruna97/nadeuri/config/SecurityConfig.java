1a1fcfddc0d7b3a5a151c2d81ea62c58f0c1a8aa
Fruna97 <kdlrlgusk@gmail.com>
2025-08-17 04:04:29 +0900

    refactor(api/member): SecurityConfig에서 직접 생성, 주입했던 의존성들을 Spring 컨테이너에 위임  - MyCustomDSL 삭제  - AuthenticationManager 빈 등록  - JwtAuthenticationFilter 빈 등록  - JwtAuthorizationFilter 빈 등록

--- server/api/src/main/java/com/github/fruna97/nadeuri/config/SecurityConfig.java
+++ server/api/src/main/java/com/github/fruna97/nadeuri/config/SecurityConfig.java
@@ -1,18 +1,17 @@
 package com.github.fruna97.nadeuri.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.intercept.AuthorizationFilter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.fruna97.nadeuri.dto.ResponseDto;
 import com.github.fruna97.nadeuri.repository.MemberRepository;
@@ -21,21 +20,35 @@ import com.github.fruna97.nadeuri.security.JwtAuthorizationFilter;
 import jakarta.servlet.http.HttpServletResponse;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
-    private final MemberRepository memberRepository;
-
-    @Autowired
-    public SecurityConfig(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
+    @Bean
+    BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
-    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(csrf -> csrf.disable())
-                .sessionManagement(
-                        sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+    AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    JwtAuthenticationFilter jwtAuthenticationFilter(AuthenticationManager authenticationManager) {
+        return new JwtAuthenticationFilter("/member/signin", authenticationManager);
+    }
+
+    @Bean
+    JwtAuthorizationFilter jwtAuthorizationFilter(MemberRepository memberRepository) {
+        return new JwtAuthorizationFilter(memberRepository);
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http,
+            JwtAuthenticationFilter jwtAuthenticationFilter,
+            JwtAuthorizationFilter jwtAuthorizationFilter) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+                .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .formLogin(formLogin -> formLogin.disable())
                 .httpBasic(httpBasic -> httpBasic.disable());
 
@@ -46,46 +59,22 @@ public class SecurityConfig {
                         .requestMatchers("/error").permitAll()
                         .anyRequest().authenticated());
 
-        http.with(new MyCustomDsl(memberRepository), dsl -> {});
+        http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+        http.addFilterBefore(jwtAuthorizationFilter, AuthorizationFilter.class);
 
         http.exceptionHandling((exceptionHandling) -> exceptionHandling
-                .authenticationEntryPoint(
-                        (request, response, authException) -> {
-                            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                .authenticationEntryPoint((request, response, authException) -> {
+                    response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
 
-                            ResponseDto<Void> responseDto = ResponseDto.<Void>builder()
-                                    .message("인증 정보가 유효하지 않습니다.")
-                                    .data(null)
-                                    .build();
-                            final ObjectMapper serializer = new ObjectMapper();
-                            response.getWriter().write(serializer.writeValueAsString(responseDto));
-                            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-                        }));
+                    ResponseDto<Void> responseDto = ResponseDto.<Void>builder()
+                            .message("인증 정보가 유효하지 않습니다.")
+                            .data(null)
+                            .build();
+                    final ObjectMapper serializer = new ObjectMapper();
+                    response.getWriter().write(serializer.writeValueAsString(responseDto));
+                    response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+                }));
 
         return http.build();
-    }
-
-    @Bean
-    BCryptPasswordEncoder bcCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    // AuthenticationManager가 생성된 후 가져오기 위해 작성한 Custom DSL
-    private class MyCustomDsl extends AbstractHttpConfigurer<MyCustomDsl, HttpSecurity> {
-
-        private final MemberRepository memberRepository;
-
-        MyCustomDsl(MemberRepository memberRepository) {
-            this.memberRepository = memberRepository;
-        }
-
-        @Override
-        public void configure(HttpSecurity http) throws Exception {
-            AuthenticationManager authenticationManager = http.getSharedObject(AuthenticationManager.class);
-            http
-                    .addFilterBefore(new JwtAuthenticationFilter("/member/signin", authenticationManager),
-                            UsernamePasswordAuthenticationFilter.class)
-                    .addFilterBefore(new JwtAuthorizationFilter(memberRepository), AuthorizationFilter.class);
-        }
     }
 }
