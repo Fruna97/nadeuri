c59314a184c3fe96629f1d4f8197b11f657231f9
Fruna97 <kdlrlgusk@gmail.com>
2025-08-21 03:36:07 +0900

    feat(api/member): Member 도메인에 UUID 필드 추가  - Member 도메인에 외부 공개용 식별자인 UUID 필드 추가  - MemberRepository에 UUID를 통한 조회 추가  - ID, UUID를 통한 조회 테스트 추가 (MemoryMemberRepositoryTest)

--- server/api/src/main/java/com/github/fruna97/nadeuri/domain/Member.java
+++ server/api/src/main/java/com/github/fruna97/nadeuri/domain/Member.java
@@ -1,7 +1,7 @@
 package com.github.fruna97.nadeuri.domain;
 
 import java.time.LocalDateTime;
-
+import java.util.UUID;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -10,6 +10,7 @@ import lombok.Getter;
 public class Member {
 
     private Long id;
+    private UUID uuid;
     private String email;
     private String password;
     private String nickname;
