5d6d9132c1e06056d95021d0bab94c274202c931
Fruna97 <kdlrlgusk@gmail.com>
2025-08-25 06:17:23 +0900

    refactor(mobile/auth): 응답 DTO 역직렬화 메서드 수정  - 필드 검증 오류 응답에 대한 역직렬화 메서드명 수정 및 로직 단순화 (ResponseDto)

--- client/mobile/lib/dto/response_dto.dart
+++ client/mobile/lib/dto/response_dto.dart
@@ -9,28 +9,9 @@ abstract class ResponseDto<T> with _$ResponseDto<T> {
 
   factory ResponseDto.fromJson(Map<String, dynamic> json, T Function(Object?) fromJsonT) => _$ResponseDtoFromJson(json, fromJsonT);
 
-  static Map<String, List<String>>? dataFromFieldValidation(Object? data) {
-    if (data == null) return null;
-
-    final Map<String, List<String>> deserializedData = <String, List<String>>{};
-
-    if (data is! Map<String, dynamic>) throw FormatException("응답 형식이 유효하지 않습니다");
-
-    for (final entry in data.entries) {
-      final (key, value) = (entry.key, entry.value);
-
-      if (value is! List<dynamic>) throw FormatException("응답 형식이 유효하지 않습니다");
-
-      final List<String> messages = <String>[];
-      for (final message in value) {
-        if (message is! String) throw FormatException("응답 형식이 유효하지 않습니다");
-
-        messages.add(message);
-      }
-      deserializedData[key] = messages;
-    }
-
-    return deserializedData;
+  static Map<String, List<String>>? dataFromFieldValidationError(Object? data) {
+    final dataWip = Map<String, dynamic>.from(data as Map);
+    return dataWip.map((k, v) => MapEntry(k, List<String>.from(v as List)));
   }
 
   static Map<String, String>? dataFromTokens(Object? data) {
