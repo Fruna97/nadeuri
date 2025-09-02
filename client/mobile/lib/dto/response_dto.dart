961f8c7815182b9d3d972da49fba10ca7f02527c
Fruna97 <kdlrlgusk@gmail.com>
2025-09-02 10:40:52 +0900

    refactor(mobile/auth): 응답 DTO 역직렬화 방식 수정
    응답 DTO의 역직렬화 방식을 임의의 메서드를 이용하는 방식에서 데이터 클래스를 이용하는 방식으로 수정
     - 데이터 클래스 추가 (TokenData, ValidationErrorData)
     - 역직렬화 메서드 삭제 (ResponseDto)

--- client/mobile/lib/dto/response_dto.dart
+++ client/mobile/lib/dto/response_dto.dart
@@ -5,16 +5,10 @@ part 'response_dto.g.dart';
 
 @Freezed(genericArgumentFactories: true)
 abstract class ResponseDto<T> with _$ResponseDto<T> {
-  const factory ResponseDto({required String message, T? data}) = _ResponseDto;
+  const factory ResponseDto({
+    required String message, 
+    T? data
+  }) = _ResponseDto;
 
   factory ResponseDto.fromJson(Map<String, dynamic> json, T Function(Object?) fromJsonT) => _$ResponseDtoFromJson(json, fromJsonT);
-
-  static Map<String, List<String>>? dataFromFieldValidationError(Object? data) {
-    final dataWip = Map<String, dynamic>.from(data as Map);
-    return dataWip.map((k, v) => MapEntry(k, List<String>.from(v as List)));
-  }
-
-  static Map<String, String>? dataFromTokens(Object? data) {
-    return Map<String, String>.from(data as Map);
-  }
 }
