694ab41386773f2fa1f3c1de023d98811468bef5
Fruna97 <kdlrlgusk@gmail.com>
2025-09-13 01:37:26 +0900

    fix(api): 빈 중복 등록 수정  - AppConfig와 @Repsitory에 의해 두 번 등록되어 있던 MemoryRefreshTokenRepository 문제 해결

--- server/api/src/main/java/com/github/fruna97/nadeuri/repository/MemoryRefreshTokenRepository.java
+++ server/api/src/main/java/com/github/fruna97/nadeuri/repository/MemoryRefreshTokenRepository.java
@@ -5,9 +5,7 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public class MemoryRefreshTokenRepository implements RefreshTokenRepository {
 
     private static Map<UUID, String> store = new ConcurrentHashMap<>();
