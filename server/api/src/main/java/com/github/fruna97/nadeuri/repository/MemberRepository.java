55330112e840ac25fe55e76e8a59a0ef8d3e5763
Fruna97 <kdlrlgusk@gmail.com>
2025-09-19 08:25:56 +0900

    feat(api/member): MemoryMemberRepository를 JPA MemberRepository로 대체  - MemoryMemberRepository 삭제 (AppConfig, MemoryMemberRepository, MemoryMemberRepositoryTest)  - MemberRepository 추상체가 JpaRepository를 상속받도록 변경 (MemberRepository)  - 회원가입 시 DataIntegrityViolationException 예외를 Catch하여 DuplicateEmailException 예외로 Rethrow 하도록 수정 (MemberServiceImpl)  - 변경사항에 관련된 테스트 수정 (JwtServiceImplTest, MemberControllerIntegrationTest, MemberServiceImplTest)

--- server/api/src/main/java/com/github/fruna97/nadeuri/repository/MemberRepository.java
+++ server/api/src/main/java/com/github/fruna97/nadeuri/repository/MemberRepository.java
@@ -2,12 +2,10 @@ package com.github.fruna97.nadeuri.repository;
 
 import java.util.Optional;
 import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
 import com.github.fruna97.nadeuri.domain.Member;
 
-public interface MemberRepository {
-    Member save(Member member);
-
-    Optional<Member> findById(Long id);
+public interface MemberRepository extends JpaRepository<Member, Long> {
 
     Optional<Member> findByUuid(UUID uuid);
 
--- server/api/src/test/java/com/github/fruna97/nadeuri/controller/MemberControllerIntegrationTest.java
+++ server/api/src/test/java/com/github/fruna97/nadeuri/controller/MemberControllerIntegrationTest.java
@@ -1,48 +1,40 @@
 package com.github.fruna97.nadeuri.controller;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Stream;
-
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.fruna97.nadeuri.dto.ResponseDto;
-import com.github.fruna97.nadeuri.repository.MemberRepository;
-import com.github.fruna97.nadeuri.repository.MemoryMemberRepository;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
 class MemberControllerIntegrationTest {
 
     @Autowired
-    MemberRepository memberRepository;
-
-    @BeforeEach
-    void beforeEach() {
-        if (memberRepository instanceof MemoryMemberRepository) {
-            MemoryMemberRepository.clear();
-        }
-    }
+    MockMvc mockMvc;
+    @Autowired
+    ObjectMapper objectMapper;
 
     @Test
-    void signUp(@Autowired TestRestTemplate restTemplate) {
+    void signUp() throws Exception {
         // Given
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
@@ -53,24 +45,20 @@ class MemberControllerIntegrationTest {
                     "nickname": "test_nickname"
                 }
                 """;
-        HttpEntity<String> request = new HttpEntity<>(requestBody, headers);
 
         // When
-        ResponseEntity<ResponseDto> response = restTemplate.exchange(
-                "/member/signup",
-                HttpMethod.POST,
-                request,
-                ResponseDto.class);
-        HttpStatusCode statusCode = response.getStatusCode();
-        ResponseDto responseBody = Optional.ofNullable(response.getBody()).get();
+        MockHttpServletResponse response = mockMvc
+                .perform(post("/member/signup")
+                    .headers(headers)
+                    .content(requestBody)).andReturn().getResponse();
+        
 
         // Then
-        assertThat(statusCode).isEqualTo(HttpStatus.OK);
-        assertThat(responseBody.getMessage()).isEqualTo("회원가입 성공");
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value()); // 회원가입이 정상적으로 이루어 지는지
     }
 
     @Test
-    void signUpWithDuplicatedEmail(@Autowired TestRestTemplate restTemplate) {
+    void signUpWithDuplicatedEmail() throws Exception {
         // Given
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
@@ -78,92 +66,62 @@ class MemberControllerIntegrationTest {
         String requestBody1 = """
                 {
                     "email": "test_eamil@test.com",
-                    "password": "test_password",
-                    "nickname": "test_nickname"
+                    "password": "test_password1",
+                    "nickname": "test_nickname1"
                 }
                 """;
-        HttpEntity<String> request1 = new HttpEntity<>(requestBody1, headers);
 
         String requestBody2 = """
                 {
                     "email": "test_eamil@test.com",
-                    "password": "test_password",
-                    "nickname": "test_nickname"
+                    "password": "test_password2",
+                    "nickname": "test_nickname2"
                 }
                 """;
-        HttpEntity<String> request2 = new HttpEntity<>(requestBody2, headers);
+
+        mockMvc.perform(post("/member/signup").headers(headers).content(requestBody1));
 
         // When
-        restTemplate.exchange(
-                "/member/signup",
-                HttpMethod.POST,
-                request1,
-                ResponseDto.class);
-
-        ResponseEntity<ResponseDto> response = restTemplate.exchange(
-                "/member/signup",
-                HttpMethod.POST,
-                request2,
-                ResponseDto.class);
-        HttpStatusCode statusCode = response.getStatusCode();
-        ResponseDto responseBody = Optional.ofNullable(response.getBody()).get();
+        MockHttpServletResponse response = mockMvc.perform(post("/member/signup").headers(headers).content(requestBody2)).andReturn().getResponse();
 
         // Then
-        assertThat(statusCode).isEqualTo(HttpStatus.CONFLICT);
-        assertThat(responseBody.getMessage()).isEqualTo("이미 존재하는 이메일 입니다: test_eamil@test.com");
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.CONFLICT.value()); // 중복 이메일 가입 시 CONFLICT 코드를 응답 하는지
     }
 
     static Stream<Arguments> invalidRequests() {
         return Stream.of(
-            // 빈 이메일
-            Arguments.of(
-                """
-                {
-                    "email": "",
-                    "password": "test_password",
-                    "nickname": "test_nickname"
-                }
-                """,
-                "유효성 검사에 실패했습니다.",
-                Map.of("email", List.of("공백일 수 없습니다"))
-            ), 
-            // 올바르지 않은 형식의 이메일
-            Arguments.of(
-                """
-                {
-                    "email": "invalid_email_format",
-                    "password": "test_password",
-                    "nickname": "test_nickname"
-                }
-                """,
-                "유효성 검사에 실패했습니다.",
-                Map.of("email", List.of("올바른 형식의 이메일 주소여야 합니다"))
-            ), 
-            // 빈 비밀번호
-            Arguments.of(
-                """
-                {
-                    "email": "test_email@test.com",
-                    "password": "",
-                    "nickname": "test_nickname"
-                }
-                """,
-                "유효성 검사에 실패했습니다.",
-                Map.of("password", List.of("공백일 수 없습니다", "비밀번호는 9자 이상 이여야 합니다"))
-            ), 
-            // 9자 미만의 비밀번호
-            Arguments.of(
-                    """
-                    {
-                        "email": "test_email@test.com",
-                        "password": "under_9",
-                        "nickname": "test_nickname"
-                    }
-                    """,
-                    "유효성 검사에 실패했습니다.",
-                    Map.of("password", List.of("비밀번호는 9자 이상 이여야 합니다"))
-            ) 
-        );
+                // 빈 이메일
+                Arguments.of("""
+                        {
+                            "email": "",
+                            "password": "test_password",
+                            "nickname": "test_nickname"
+                        }
+                        """, "유효성 검사에 실패했습니다.", Set.of("email")),
+                // 올바르지 않은 형식의 이메일
+                Arguments.of("""
+                        {
+                            "email": "invalid_email_format",
+                            "password": "test_password",
+                            "nickname": "test_nickname"
+                        }
+                        """, "유효성 검사에 실패했습니다.", Set.of("email")),
+                // 빈 비밀번호
+                Arguments.of("""
+                        {
+                            "email": "test_email@test.com",
+                            "password": "",
+                            "nickname": "test_nickname"
+                        }
+                        """, "유효성 검사에 실패했습니다.", Set.of("password")),
+                // 9자 미만의 비밀번호
+                Arguments.of("""
+                        {
+                            "email": "test_email@test.com",
+                            "password": "under_9",
+                            "nickname": "test_nickname"
+                        }
+                        """, "유효성 검사에 실패했습니다.", Set.of("password")));
     }
 
     @ParameterizedTest
@@ -171,28 +129,22 @@ class MemberControllerIntegrationTest {
     void signUpWithInvalidRequests(
             String requestBody,
             String expectedMessage,
-            Map<String, List<String>> expectedData,
-            @Autowired TestRestTemplate restTemplate) {
+            Set<String> expectedData) throws Exception {
         // Given
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
-        HttpEntity<String> request = new HttpEntity<String>(requestBody, headers);
 
         // When
-        ResponseEntity<ResponseDto> response = restTemplate.exchange(
-                "/member/signup",
-                HttpMethod.POST,
-                request,
-                ResponseDto.class);
-        HttpStatusCode statusCode = response.getStatusCode();
-        String message = Optional.ofNullable(response.getBody()).get().getMessage();
-        Map<String, List<String>> data = (Map<String, List<String>>) Optional.ofNullable(response.getBody()).get().getData();
+        MockHttpServletResponse response = mockMvc.perform(post("/member/signup").headers(headers).content(requestBody)).andReturn().getResponse();
 
         // Then
-        assertThat(statusCode).isEqualTo(HttpStatus.BAD_REQUEST);
-        assertThat(message).isEqualTo(expectedMessage);
-        data.keySet().forEach(key -> {
-            assertThat(Set.copyOf(data.get(key))).isEqualTo(Set.copyOf(expectedData.get(key)));
-        });
+        ResponseDto<Map<String, List<String>>> responseDto = objectMapper.readValue(response.getContentAsString(), new TypeReference<ResponseDto<Map<String, List<String>>>>() {});
+        int status = response.getStatus();
+        String message = responseDto.getMessage();
+        Map<String, List<String>> data = responseDto.getData();
+
+        assertThat(status).isEqualTo(HttpStatus.BAD_REQUEST.value()); // 요청 본문 검증 실패 시 BAD_REQUEST 코드를 응답 하는지
+        assertThat(message).isEqualTo(expectedMessage); // 응답 본문의 메시지가 "유효성 검사에 실패했습니다." 인지
+        assertThat(data.keySet()).isEqualTo(expectedData); // 응답 본문의 데이터가 유효성 검증에 실패한 필드를 포함하는지
     }
 }
