3e61b09dbcc40e75a062e00d3b9f74bfccfa86ee
Fruna97 <kdlrlgusk@gmail.com>
2025-09-24 11:57:25 +0900

    feat(api/nadeuri): NadeuriService 인터페이스 정의 및 구현체 생성

---
+++ server/api/src/main/java/com/github/fruna97/nadeuri/service/NadeuriService.java
@@ -0,0 +1,5 @@
+package com.github.fruna97.nadeuri.service;
+
+public interface NadeuriService {
+
+}
---
+++ server/api/src/main/java/com/github/fruna97/nadeuri/service/NadeuriServiceImpl.java
@@ -0,0 +1,8 @@
+package com.github.fruna97.nadeuri.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class NadeuriServiceImpl implements NadeuriService {
+
+}
