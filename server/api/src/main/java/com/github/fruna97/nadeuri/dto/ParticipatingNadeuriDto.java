8d1fe8624319f98db7f4067c5940f5a61793b06f
Fruna97 <kdlrlgusk@gmail.com>
2025-09-24 17:24:52 +0900

    test(api/nadeuri): NadeuriService 단위 테스트 추가  - Nadeuri 생성, 참가 중인 Nadeuri 목록 조회 테스트 (NadeuriServiceImplTest)  - Getter 추가 (ParticipatingNadeuriDto)

--- server/api/src/main/java/com/github/fruna97/nadeuri/dto/ParticipatingNadeuriDto.java
+++ server/api/src/main/java/com/github/fruna97/nadeuri/dto/ParticipatingNadeuriDto.java
@@ -1,8 +1,10 @@
 package com.github.fruna97.nadeuri.dto;
 
 import lombok.Builder;
+import lombok.Getter;
 
 @Builder
+@Getter
 public class ParticipatingNadeuriDto {
 
     private String title;
---
+++ server/api/src/test/java/com/github/fruna97/nadeuri/service/NadeuriServiceImplTest.java
@@ -0,0 +1,112 @@
+package com.github.fruna97.nadeuri.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.transaction.annotation.Transactional;
+import com.github.fruna97.nadeuri.domain.Member;
+import com.github.fruna97.nadeuri.domain.Nadeuri;
+import com.github.fruna97.nadeuri.dto.ParticipatingNadeuriDto;
+import com.github.fruna97.nadeuri.repository.MemberRepository;
+import com.github.fruna97.nadeuri.repository.NadeuriRepository;
+import com.github.fruna97.nadeuri.security.PrincipalDetails;
+
+@ExtendWith(MockitoExtension.class)
+@Transactional
+public class NadeuriServiceImplTest {
+
+    @Mock
+    NadeuriRepository nadeuriRepository;
+
+    @Mock
+    MemberRepository memberRepository;
+
+    @InjectMocks
+    NadeuriServiceImpl nadeuriServiceImpl;
+
+    @Captor
+    ArgumentCaptor<Nadeuri> nadeuriCaptor;
+
+    @Test
+    void createNadeuri() {
+        // Given
+        Member member = Member.builder()
+                .id(1L)
+                .email("test_email@test.com")
+                .password("test_password")
+                .nickname("test_nickname").build();
+        PrincipalDetails principalDetails = new PrincipalDetails(member);
+        when(memberRepository.getReferenceById(1L)).thenReturn(member);
+
+        String title = "test_title";
+        Nadeuri savedNadeuri = Nadeuri.builder()
+                .id(97L)
+                .title(title).build();
+        when(nadeuriRepository.save(any(Nadeuri.class))).thenReturn(savedNadeuri);
+
+        // When
+        Nadeuri result = nadeuriServiceImpl.createNadeuri(principalDetails, title);
+
+        // Then
+        assertThat(result).isEqualTo(savedNadeuri); // 저장된 객체를 그대로 반환 하는지
+
+        verify(nadeuriRepository).save(nadeuriCaptor.capture());
+        Nadeuri capturedNadeuri = nadeuriCaptor.getValue();
+        assertThat(capturedNadeuri.getTitle()).isEqualTo(title); // 주어진 제목을 그대로 저장하는지
+        assertThat(capturedNadeuri.getOwner()).isEqualTo(member); // 인증 회원을 Owner로 지정하는지
+        assertThat(capturedNadeuri.getMembers()).contains(member); // 인증 회원을 참여 회원 목록에 담는지
+    }
+
+    @Test
+    void getParticipatingNadeuris() {
+        // Given
+        Set<Nadeuri> participatingNadeuris = new HashSet<>();
+        Member member = Member.builder()
+                .id(1L)
+                .email("test_email@test.com")
+                .password("test_password")
+                .nickname("test_nickname")
+                .participatingNadeuris(participatingNadeuris).build();
+        PrincipalDetails principalDetails = new PrincipalDetails(member);
+
+        String title1 = "title1";
+        String title2 = "title2";
+        Nadeuri nadeuri1 = Nadeuri.builder()
+                .id(97L)
+                .title(title1)
+                .owner(member)
+                .members(Set.of(member)).build();
+        Nadeuri nadeuri2 = Nadeuri.builder()
+                .id(98L)
+                .title(title2)
+                .owner(member)
+                .members(Set.of(member)).build();
+        participatingNadeuris.add(nadeuri1);
+        participatingNadeuris.add(nadeuri2);
+
+        when(memberRepository.findById(1L)).thenReturn(Optional.of(member));
+
+        // When
+        Set<ParticipatingNadeuriDto> result = nadeuriServiceImpl.getParticipatingNadeuris(principalDetails);
+
+        // Then
+        assertThat(result).hasSize(2); // 참가 Nadeuri 목록의 개수가 정확한지
+
+        Set<String> titles = result.stream()
+                .map(participatingNadeuriDto -> participatingNadeuriDto.getTitle())
+                .collect(Collectors.toSet());
+        assertThat(titles).containsExactlyInAnyOrder(title1, title2); // 변환된 DTO가 제목을 그대로 가지고 있는지
+    }
+}
