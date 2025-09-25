3765deee6666368c2882a0f39f87fc7b147185b0
Fruna97 <kdlrlgusk@gmail.com>
2025-09-25 23:09:40 +0900

    feat(api/nadeuri): NadeuriController 추가  - Nadeuri 생성 요청을 받기 위해 DTO 생성 (CreateNadeuriDto)  - Nadeuri 생성 엔드포인트 추가 (NadeuriController)  - 참가 중인 Nadeuri 목록 조회 엔드포인트 추가 (NadeuriController)

---
+++ server/api/src/main/java/com/github/fruna97/nadeuri/controller/NadeuriController.java
@@ -0,0 +1,52 @@
+package com.github.fruna97.nadeuri.controller;
+
+import java.util.Set;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import com.github.fruna97.nadeuri.dto.CreateNadeuriDto;
+import com.github.fruna97.nadeuri.dto.ParticipatingNadeuriDto;
+import com.github.fruna97.nadeuri.dto.ResponseDto;
+import com.github.fruna97.nadeuri.security.PrincipalDetails;
+import com.github.fruna97.nadeuri.service.NadeuriService;
+
+
+
+@Controller
+public class NadeuriController {
+
+    private final NadeuriService nadeuriService;
+
+    @Autowired
+    public NadeuriController(NadeuriService nadeuriService) {
+        this.nadeuriService = nadeuriService;
+    }
+
+    @PostMapping("/nadeuri/create")
+    public ResponseEntity<ResponseDto<Void>> createNadeuri(@AuthenticationPrincipal PrincipalDetails principalDetails, @RequestBody CreateNadeuriDto createNadeuriDto) {
+        String title = createNadeuriDto.getTitle();
+        
+        nadeuriService.createNadeuri(principalDetails, title);
+
+        return ResponseEntity
+                .ok()
+                .body(ResponseDto.<Void>builder()
+                        .message("Nadeuri가 생성되었습니다.")
+                        .data(null).build());
+    }
+
+    @GetMapping("/nadeuri/participating")
+    public ResponseEntity<ResponseDto<Set<ParticipatingNadeuriDto>>> participatingNadeuris(@AuthenticationPrincipal PrincipalDetails principalDetails) {
+        Set<ParticipatingNadeuriDto> participatingNadeuris = nadeuriService.getParticipatingNadeuris(principalDetails);
+
+        return ResponseEntity
+                .ok()
+                .body(ResponseDto.<Set<ParticipatingNadeuriDto>>builder()
+                        .message("성공적으로 Nadeuri가 조회되었습니다.")
+                        .data(participatingNadeuris).build());
+    }
+}
---
+++ server/api/src/main/java/com/github/fruna97/nadeuri/dto/CreateNadeuriDto.java
@@ -0,0 +1,11 @@
+package com.github.fruna97.nadeuri.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class CreateNadeuriDto {
+
+    private String title;
+}
