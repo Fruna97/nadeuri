f9d16ae82d5592afbb0e6a134f8903e9fcd63996
Fruna97 <kdlrlgusk@gmail.com>
2025-10-06 16:50:32 +0900

    feat(mobile): 회원가입 Service 메서드 추가

--- client/mobile/lib/data/service/api_client.dart
+++ client/mobile/lib/data/service/api_client.dart
@@ -1,4 +1,12 @@
+import 'dart:convert';
+import 'dart:developer';
+import 'dart:io';
+
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:http/http.dart' as http;
+import 'package:mobile/data/service/model/sign_up_request/sign_up_request.dart';
+import 'package:mobile/data/service/model/sign_up_response/sign_up_response.dart';
+import 'package:mobile/result.dart';
 
 class ApiClient {
   final String _host;
@@ -15,4 +23,47 @@ class ApiClient {
        _port = port,
        _baseHeaders = baseHeaders,
        _flutterSecureStorage = flutterSecureStorage;
+
+  Future<Result<SignUpResponse>> signUp(SignUpRequest signUpRequest) async {
+    String endpoint = "/member/signup";
+    http.Response? response;
+    try {
+      response = await http
+          .post(Uri.parse("$_host:$_port$endpoint"), headers: _baseHeaders, body: jsonEncode(signUpRequest.toJson()))
+          .timeout(
+            const Duration(seconds: 5),
+            onTimeout: () {
+              return http.Response(
+                jsonEncode({"message": "타임아웃", "data": null}),
+                HttpStatus.requestTimeout,
+                headers: {"content-type": "application/json;charset=UTF-8"},
+              );
+            },
+          );
+    } catch (e) {
+      log(e.toString());
+      return Result.error(SignUpResponse.unknownError());
+    }
+
+    int statusCode = response.statusCode;
+    dynamic decodedBody = jsonDecode(response.body);
+    String? message = decodedBody["message"];
+    Map<String, dynamic>? data = decodedBody["data"];
+    log("Response summary (StatusCode: $statusCode, Message: $message)");
+
+    data ??= <String, dynamic>{};
+    data["runtimeType"] = switch (statusCode) {
+      HttpStatus.ok => "registered",
+      HttpStatus.unprocessableEntity => "validationError",
+      HttpStatus.conflict => "duplicateEmailError",
+      _ => "unknownError",
+    };
+    SignUpResponse signUpResponse = SignUpResponse.fromJson(data);
+
+    if (signUpResponse is Registered) {
+      return Result.ok(signUpResponse);
+    } else {
+      return Result.error(signUpResponse);
+    }
+  }
 }
