19bd1dbdbbb3659ad596cabdeb84a55e4c9783af
Fruna97 <kdlrlgusk@gmail.com>
2025-10-09 07:17:14 +0900

    feat(mobile): 인증 관련 데이터 관리를 위해 Repository 클래스(AuthRepository) 생성  - 회원가입 메서드 추가

---
+++ client/mobile/lib/data/repository/auth_repository.dart
@@ -0,0 +1,37 @@
+import 'dart:developer';
+
+import 'package:mobile/data/service/api_client.dart';
+import 'package:mobile/data/service/model/sign_up_request/sign_up_request.dart';
+import 'package:mobile/data/service/model/sign_up_response/sign_up_response.dart';
+import 'package:mobile/result.dart';
+
+class AuthRepository {
+  final ApiClient _apiClient;
+  
+  AuthRepository({required ApiClient apiClient}) : _apiClient = apiClient;
+
+  Future<Result<SignUpResponse>> signUp({
+    required String email,
+    required String password,
+    required String nickname,
+  }) async {
+    SignUpRequest signUpRequest = SignUpRequest(email: email, password: password, nickname: nickname);
+
+    final Result<SignUpResponse> result = await _apiClient.signUp(signUpRequest);
+    switch (result) {
+      case Ok<SignUpResponse> _:
+        log("Result is Ok");
+      case Error<SignUpResponse> _:
+        SignUpResponse error = result.error;
+        switch (error) {
+          case ValidationError _:
+            log("Result is ValidationError: $error");
+          case DuplicateEmailError _:
+            log("Result is DuplicateEmailError");
+          case UnknownError _:
+            log("Result is UnknownError");
+        }
+    }
+    return result;
+  }
+}
