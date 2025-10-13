ddb4f085297658766e72bde7ab6d3f46bba660f3
Fruna97 <kdlrlgusk@gmail.com>
2025-10-14 06:33:33 +0900

    refactor(mobile): Service의 http 사용 코드를 dio 사용으로 마이그레이션

--- client/mobile/lib/data/service/api_client.dart
+++ client/mobile/lib/data/service/api_client.dart
@@ -1,113 +1,111 @@
-import 'dart:convert';
 import 'dart:developer';
 import 'dart:io';
 
+import 'package:dio/dio.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import 'package:http/http.dart' as http;
 import 'package:mobile/data/service/model/api_error/api_error.dart';
 import 'package:mobile/data/service/model/sign_in_request/sign_in_request.dart';
 import 'package:mobile/data/service/model/sign_in_response/sign_in_response.dart';
 import 'package:mobile/data/service/model/sign_up_request/sign_up_request.dart';
 import 'package:mobile/utils/result.dart';
 
+enum RequestMethod { signUp, signIn }
+
 class ApiClient {
-  final String _host;
-  final int _port;
-  final Map<String, String> _baseHeaders;
   final FlutterSecureStorage _flutterSecureStorage;
+  late final Dio _dioWithoutToken;
+
+  final Duration connectTimeout = Duration(seconds: 5);
+  final Duration receiveTimeout = Duration(seconds: 5);
 
   ApiClient({
     required String host,
     required int port,
     required Map<String, String> baseHeaders,
     required FlutterSecureStorage flutterSecureStorage,
-  }) : _host = host,
-       _port = port,
-       _baseHeaders = baseHeaders,
-       _flutterSecureStorage = flutterSecureStorage;
+  }) : _flutterSecureStorage = flutterSecureStorage {
+    _dioWithoutToken = Dio(
+      BaseOptions(
+        baseUrl: "$host:$port",
+        headers: baseHeaders,
+        connectTimeout: connectTimeout,
+        receiveTimeout: receiveTimeout,
+      ),
+    );
+  }
 
   Future<Result<void>> signUp(SignUpRequest signUpRequest) async {
     final String endpoint = "/member/signup";
-    final http.Response? response;
     try {
-      response = await http
-          .post(Uri.parse("$_host:$_port$endpoint"), headers: _baseHeaders, body: jsonEncode(signUpRequest.toJson()))
-          .timeout(
-            const Duration(seconds: 5),
-            onTimeout: () {
-              return http.Response(
-                jsonEncode({"message": "타임아웃", "data": null}),
-                HttpStatus.requestTimeout,
-                headers: {"content-type": "application/json;charset=UTF-8"},
-              );
-            },
-          );
+      final Response response = await _dioWithoutToken.post(endpoint, data: signUpRequest.toJson());
+      final int statusCode = response.statusCode!;
+      final dynamic body = response.data;
+      final String? message = body["message"];
+      log("${RequestMethod.signUp.name} response summary (StatusCode: $statusCode, Message: $message)");
+
+      return Result.ok(null);
+    } on DioException catch (e) {
+      return _handleOnDioException<void>(e, RequestMethod.signUp);
     } catch (e) {
       log(e.toString());
       return Result.error(ApiError.unknownError());
     }
-
-    final int statusCode = response.statusCode;
-    final dynamic decodedBody = jsonDecode(response.body);
-    final String? message = decodedBody["message"];
-    log("Sign-Up response summary (StatusCode: $statusCode, Message: $message)");
-
-    if (statusCode == HttpStatus.ok) {
-      return Result.ok(null);
-    }
-
-    Map<String, dynamic>? data = decodedBody["data"];
-    data ??= <String, dynamic>{};
-    data["runtimeType"] = switch (statusCode) {
-      HttpStatus.requestTimeout => "requestTimeout",
-      HttpStatus.conflict => "duplicateEmail",
-      HttpStatus.unprocessableEntity => "validationError",
-      _ => "unknownError",
-    };
-    final ApiError apiError = ApiError.fromJson(data);
-    return Result.error(apiError);
   }
 
   Future<Result<SignInResponse>> signIn(SignInRequest signInRequest) async {
     final String endpoint = "/member/signin";
-    final http.Response? response;
     try {
-      response = await http
-          .post(Uri.parse("$_host:$_port$endpoint"), headers: _baseHeaders, body: jsonEncode(signInRequest.toJson()))
-          .timeout(
-            const Duration(seconds: 5),
-            onTimeout: () {
-              return http.Response(
-                jsonEncode({"message": "타임아웃", "data": null}),
-                HttpStatus.requestTimeout,
-                headers: {"content-type": "application/json;charset=UTF-8"},
-              );
-            },
-          );
+      final Response response = await _dioWithoutToken.post(endpoint, data: signInRequest.toJson());
+      final int statusCode = response.statusCode!;
+      final dynamic body = response.data;
+      final String? message = body["message"];
+      log("${RequestMethod.signIn.name} response summary (StatusCode: $statusCode, Message: $message)");
+
+      final Map<String, dynamic> data = body["data"];
+      final SignInResponse signInResponse = SignInResponse.fromJson(data);
+      return Result.ok(signInResponse);
+    } on DioException catch (e) {
+      return _handleOnDioException<SignInResponse>(e, RequestMethod.signIn);
     } catch (e) {
       log(e.toString());
       return Result.error(ApiError.unknownError());
     }
+  }
 
-    final int statusCode = response.statusCode;
-    final dynamic decodedBody = jsonDecode(response.body);
-    final String? message = decodedBody["message"];
-    log("Sign-In response summary (StatusCode: $statusCode, Message: $message)");
-
-    if (statusCode == HttpStatus.ok) {
-      final Map<String, dynamic> data = decodedBody["data"];
-      final SignInResponse signInResponse = SignInResponse.fromJson(data);
-      return Result.ok(signInResponse);
+  Result<T> _handleOnDioException<T>(DioException e, RequestMethod requestMethod) {
+    if (e.type == DioExceptionType.connectionTimeout ||
+        e.type == DioExceptionType.receiveTimeout ||
+        e.type == DioExceptionType.sendTimeout) {
+      return Result.error(ApiError.requestTimeout());
     }
 
-    Map<String, dynamic>? data = decodedBody["data"];
+    final Response? response = e.response;
+    if (response == null) {
+      return Result.error(ApiError.unknownError());
+    }
+
+    final int statusCode = response.statusCode!;
+    final dynamic body = response.data;
+    final String? message = body["message"];
+    log("${requestMethod.name} response summary (StatusCode: $statusCode, Message: $message)");
+
+    Map<String, dynamic>? data = body["data"];
     data ??= <String, dynamic>{};
-    data["runtimeType"] = switch (statusCode) {
-      HttpStatus.unauthorized => "unauthorized",
-      HttpStatus.requestTimeout => "requestTimeout",
-      HttpStatus.unprocessableEntity => "validationError",
-      _ => "unknownError",
-    };
+
+    switch (requestMethod) {
+      case RequestMethod.signUp:
+        data["runtimeType"] = switch (statusCode) {
+          HttpStatus.conflict => "duplicateEmail",
+          HttpStatus.unprocessableEntity => "validationError",
+          _ => "unknownError",
+        };
+      case RequestMethod.signIn:
+        data["runtimeType"] = switch (statusCode) {
+          HttpStatus.unauthorized => "unauthorized",
+          HttpStatus.unprocessableEntity => "validationError",
+          _ => "unknownError",
+        };
+    }
     final ApiError apiError = ApiError.fromJson(data);
     return Result.error(apiError);
   }
