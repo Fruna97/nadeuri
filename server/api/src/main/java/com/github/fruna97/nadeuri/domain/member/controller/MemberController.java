33a1985dd8662de4f1cf9f0df7e16f4f9e6fdd87
Fruna97 <kdlrlgusk@gmail.com>
2025-11-13 21:05:50 +0900

    feat(api/member): 내 프로필 조회 엔드포인트 추가  - 내 프로필 조회 엔드포인트 추가 (MemberController)  - 관련 테스트 추가 (MemberControllerTest)  - 테스트 주석 문구 수정 (AuthControllerTest)

--- server/api/src/main/java/com/github/fruna97/nadeuri/domain/member/controller/MemberController.java
+++ server/api/src/main/java/com/github/fruna97/nadeuri/domain/member/controller/MemberController.java
@@ -2,14 +2,17 @@ package com.github.fruna97.nadeuri.domain.member.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import com.github.fruna97.nadeuri.common.dto.ResponseDto;
+import com.github.fruna97.nadeuri.domain.member.dto.MemberSummaryResponse;
 import com.github.fruna97.nadeuri.domain.member.dto.SignUpRequest;
 import com.github.fruna97.nadeuri.domain.member.service.MemberService;
+import com.github.fruna97.nadeuri.security.PrincipalDetails;
 import jakarta.validation.Valid;
-
 
 @RestController
 public class MemberController {
@@ -31,5 +34,17 @@ public class MemberController {
                         .message("회원가입 성공")
                         .data(null)
                         .build());
-    }    
+    }
+
+    @GetMapping("/member")
+    public ResponseEntity<ResponseDto<MemberSummaryResponse>> getMyProfile(
+            @AuthenticationPrincipal PrincipalDetails principalDetails) {
+        MemberSummaryResponse memberSummaryResponse = memberService.getMyProfile(principalDetails);
+
+        return ResponseEntity
+                .ok()
+                .body(ResponseDto.<MemberSummaryResponse>builder()
+                        .message("프로필 조회 성공")
+                        .data(memberSummaryResponse).build());
+    }
 }
--- server/api/src/test/java/com/github/fruna97/nadeuri/domain/member/controller/MemberControllerTest.java
+++ server/api/src/test/java/com/github/fruna97/nadeuri/domain/member/controller/MemberControllerTest.java
@@ -1,10 +1,13 @@
 package com.github.fruna97.nadeuri.domain.member.controller;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import java.util.UUID;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -13,9 +16,11 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import com.github.fruna97.nadeuri.common.dto.ResponseDto;
+import com.github.fruna97.nadeuri.domain.member.dto.MemberSummaryResponse;
 import com.github.fruna97.nadeuri.domain.member.dto.SignUpRequest;
 import com.github.fruna97.nadeuri.domain.member.service.MemberService;
 import com.github.fruna97.nadeuri.exception.DuplicateEmailException;
+import com.github.fruna97.nadeuri.security.PrincipalDetails;
 
 @ExtendWith(MockitoExtension.class)
 class MemberControllerTest {
@@ -61,5 +66,33 @@ class MemberControllerTest {
         // When
         // Then
         assertThrows(DuplicateEmailException.class, () -> memberController.signUp(signUpRequest)); // [MemberService.signUp]에서 발생한 중복 이메일 예외를 그대로 전파하는지
+    }
+
+    @Test
+    void getMyProfile() {
+        // Given
+        long id = 0L;
+        UUID uuid = UUID.randomUUID();
+        String email = "test_email@test.com";
+        String password = "test_password";
+        String nickname = "test_nickname";
+        String profileImageUrl = "test_url";
+        PrincipalDetails principalDetails = new PrincipalDetails(id, uuid, email, password);
+
+        MemberSummaryResponse memberSummaryResponse = MemberSummaryResponse.builder()
+                .uuid(uuid)
+                .email(email)
+                .nickname(nickname)
+                .profileImageUrl(profileImageUrl).build();
+        when(memberService.getMyProfile(principalDetails)).thenReturn(memberSummaryResponse);
+
+        // When
+        ResponseEntity<ResponseDto<MemberSummaryResponse>> result = memberController.getMyProfile(principalDetails);
+
+        // Then
+        assertThat(result.getStatusCode()).isEqualTo(HttpStatus.OK); // 200 OK를 반환하는지
+        ResponseDto<MemberSummaryResponse> body = result.getBody();
+        assertNotNull(body); // 응답 본문을 담고있는지
+        assertThat(body.getData()).isEqualTo(memberSummaryResponse); // 응답 본문의 데이터가 [MemberService.getMyProfile]이 반환한 [MemberSummaryResponse]와 동일한지
     }
 }
