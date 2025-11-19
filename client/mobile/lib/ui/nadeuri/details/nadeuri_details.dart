55bc37b0b8ba5a57889233021eab92ecea7e5d84
Fruna97 <kdlrlgusk@gmail.com>
2025-11-19 17:09:56 +0900

    feat(mobile/nadeuri): Nadeuri 상세 화면 목업 추가

---
+++ client/mobile/lib/ui/nadeuri/details/nadeuri_details.dart
@@ -0,0 +1,244 @@
+import 'package:flutter/material.dart';
+import 'package:mobile/ui/nadeuri/details/nadeuri_details_view_model.dart';
+
+class NadeuriDetailsPage extends StatelessWidget {
+  final NadeuriDetailsViewModel _nadeuriDetailsViewModel;
+
+  const NadeuriDetailsPage({super.key, required NadeuriDetailsViewModel nadeuriDetailsViewModel})
+    : _nadeuriDetailsViewModel = nadeuriDetailsViewModel;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: ListView(
+          padding: EdgeInsets.symmetric(horizontal: 20.0),
+          children: <Widget>[
+            SizedBox(height: 32.0),
+            _NadeuriTitleSection(),
+            Divider(height: 32.0),
+            _ChatSection(),
+            Divider(height: 32.0),
+            _PlanSection(),
+            Divider(height: 32.0),
+            _PhotoSection(),
+            Divider(height: 32.0),
+            _SettlementSection(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _NadeuriTitleSection extends StatefulWidget {
+  const _NadeuriTitleSection({super.key});
+
+  @override
+  State<_NadeuriTitleSection> createState() => _NadeuriTitleSectionState();
+}
+
+class _NadeuriTitleSectionState extends State<_NadeuriTitleSection> {
+  final TextEditingController _nadeuriTitleController = TextEditingController();
+  final FocusNode _nadeuriTitleFocusNode = FocusNode();
+  bool _nadeuriTextFieldReadOnly = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: TextField(
+            onSubmitted: (value) {
+              setState(() {
+                _nadeuriTextFieldReadOnly = true;
+              });
+            },
+            onTapOutside: (event) {
+              setState(() {
+                _nadeuriTextFieldReadOnly = true;
+              });
+            },
+            controller: _nadeuriTitleController,
+            focusNode: _nadeuriTitleFocusNode,
+            decoration: InputDecoration(hintText: "나들이 제목을 지정해보세요!", border: InputBorder.none),
+            style: TextStyle(fontSize: 20.0),
+            readOnly: _nadeuriTextFieldReadOnly,
+          ),
+        ),
+        IconButton(
+          onPressed: () {
+            setState(() {
+              _nadeuriTextFieldReadOnly = !_nadeuriTextFieldReadOnly;
+              if (_nadeuriTextFieldReadOnly) {
+                _nadeuriTitleFocusNode.unfocus();
+              } else {
+                _nadeuriTitleFocusNode.requestFocus();
+              }
+            });
+          },
+          icon: Icon(Icons.edit),
+        ),
+      ],
+    );
+  }
+}
+
+class _ChatSection extends StatelessWidget {
+  const _ChatSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {},
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 4.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("채팅", style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold)),
+            SizedBox(height: 12.0),
+            Row(
+              children: <Widget>[
+                CircleAvatar(
+                  foregroundImage: NetworkImage(""),
+                  onForegroundImageError: (exception, stackTrace) {},
+                  radius: 20,
+                  child: Icon(Icons.person, size: 30, color: Colors.blue),
+                ),
+                SizedBox(width: 8.0),
+                CircleAvatar(
+                  foregroundImage: NetworkImage(""),
+                  onForegroundImageError: (exception, stackTrace) {},
+                  radius: 20,
+                  child: Icon(Icons.person, size: 30, color: Colors.blue),
+                ),
+                SizedBox(width: 8.0),
+                CircleAvatar(
+                  foregroundImage: NetworkImage(""),
+                  onForegroundImageError: (exception, stackTrace) {},
+                  radius: 20,
+                  child: Icon(Icons.person, size: 30, color: Colors.blue),
+                ),
+                SizedBox(width: 8.0),
+                CircleAvatar(
+                  foregroundImage: NetworkImage(""),
+                  onForegroundImageError: (exception, stackTrace) {},
+                  radius: 20,
+                  child: Icon(Icons.person, size: 30, color: Colors.blue),
+                ),
+                SizedBox(width: 8.0),
+                CircleAvatar(
+                  foregroundImage: NetworkImage(""),
+                  onForegroundImageError: (exception, stackTrace) {},
+                  radius: 20,
+                  child: Icon(Icons.person, size: 30, color: Colors.blue),
+                ),
+                SizedBox(width: 8.0),
+                Spacer(),
+                Icon(Icons.more_horiz, size: 16),
+              ],
+            ),
+            SizedBox(height: 12.0),
+            Text("최근 채팅 메시지"),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _PlanSection extends StatelessWidget {
+  const _PlanSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text("일정들", style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold)),
+        SizedBox(height: 8.0),
+        _plan("서울숲", DateTime(2025, 06, 05, 14), DateTime(2025, 06, 05, 15)),
+        _plan("여의도", DateTime(2025, 06, 06, 14), DateTime(2025, 06, 08, 15)),
+      ],
+    );
+  }
+
+  InkWell _plan(String? planTitle, DateTime startAt, DateTime endAt) {
+    return InkWell(
+      onTap: () {},
+      child: Padding(
+        padding: const EdgeInsets.all(4.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(planTitle ?? ""),
+            Row(
+              children: DateUtils.isSameDay(startAt, endAt)
+                  ? <Widget>[
+                      Text(
+                        "${startAt.year}.${startAt.month.toString().padLeft(2, "0")}.${startAt.day.toString().padLeft(2, "0")}",
+                      ),
+                      Spacer(),
+                      Text(
+                        "${startAt.hour.toString().padLeft(2, "0")}:${startAt.minute.toString().padLeft(2, "0")} ~ ${endAt.hour.toString().padLeft(2, "0")}:${endAt.minute.toString().padLeft(2, "0")}",
+                      ),
+                    ]
+                  : <Widget>[
+                      Text(
+                        "${startAt.year}.${startAt.month.toString().padLeft(2, "0")}.${startAt.day.toString().padLeft(2, "0")} ${startAt.hour.toString().padLeft(2, "0")}:${startAt.minute.toString().padLeft(2, "0")}",
+                      ),
+                      Spacer(),
+                      Text("~"),
+                      Spacer(),
+                      Text(
+                        "${endAt.year}.${endAt.month.toString().padLeft(2, "0")}.${endAt.day.toString().padLeft(2, "0")} ${endAt.hour.toString().padLeft(2, "0")}:${endAt.minute.toString().padLeft(2, "0")}",
+                      ),
+                    ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _PhotoSection extends StatelessWidget {
+  const _PhotoSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {},
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 4.0),
+        child: Row(
+          children: <Widget>[
+            Text("사진 ", style: TextStyle(fontSize: 20.0)),
+            Icon(Icons.arrow_forward_ios, size: 18.0),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _SettlementSection extends StatelessWidget {
+  const _SettlementSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {},
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 4.0),
+        child: Row(
+          children: <Widget>[
+            Text("정산 ", style: TextStyle(fontSize: 20.0)),
+            Icon(Icons.arrow_forward_ios, size: 18.0),
+          ],
+        ),
+      ),
+    );
+  }
+}
