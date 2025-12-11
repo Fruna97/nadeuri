30c02ef03f45ae1f204426c5592373f9477c7dbf
Fruna97 <kdlrlgusk@gmail.com>
2025-12-12 05:46:31 +0900

    feat(api/nadeuri): 일정 JPA 리포지토리 정의

---
+++ server/api/src/main/java/com/github/fruna97/nadeuri/domain/nadeuri/repository/PlanRepository.java
@@ -0,0 +1,8 @@
+package com.github.fruna97.nadeuri.domain.nadeuri.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.github.fruna97.nadeuri.domain.nadeuri.model.Plan;
+
+public interface PlanRepository extends JpaRepository<Plan, Long> {
+
+}
