01141ec16d72084bc96cf5ade4996819da456cf8
Fruna97 <kdlrlgusk@gmail.com>
2026-01-06 03:14:09 +0900

    feat(api/nadeuri): 일정 갱신용 DTO 생성

---
+++ server/api/src/main/java/com/github/fruna97/nadeuri/domain/nadeuri/dto/UpdatePlanRequest.java
@@ -0,0 +1,32 @@
+package com.github.fruna97.nadeuri.domain.nadeuri.dto;
+
+import java.time.LocalDateTime;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+public class UpdatePlanRequest {
+
+    @NotNull
+    @Size(max = 100)
+    private String planTitle;
+
+    private String googlePlacesId;
+
+    private Double latitude;
+
+    private Double longitude;
+
+    @NotNull
+    private LocalDateTime startAt;
+
+    @NotNull
+    private LocalDateTime endAt;
+}
