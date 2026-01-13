3db05dad140d94ce97e0443084f87fda7a0569b3
Fruna97 <kdlrlgusk@gmail.com>
2026-01-13 22:36:53 +0900

    feat(mobile/nadeuri): 일정 생성 리포지토리 메서드 추가

--- client/mobile/lib/data/repository/nadeuri_repository.dart
+++ client/mobile/lib/data/repository/nadeuri_repository.dart
@@ -5,7 +5,9 @@ import 'package:mobile/data/service/model/api_error/api_error.dart';
 import 'package:mobile/data/service/model/local_error/local_error.dart';
 import 'package:mobile/data/service/model/nadeuri/nadeuri_api_model.dart';
 import 'package:mobile/data/service/model/nadeuri/update_nadeuri_api_model.dart';
+import 'package:mobile/data/service/model/plan/plan_api_model.dart';
 import 'package:mobile/domain/model/nadeuri/nadeuri.dart';
+import 'package:mobile/domain/model/plan/plan.dart';
 import 'package:mobile/utils/result.dart';
 
 class NadeuriRepository {
@@ -102,6 +104,33 @@ class NadeuriRepository {
         log("Result is Ok: ${result.value}", name: _logTag);
         return Result.ok(result.value.toNadeuri());
       case Error<NadeuriApiModel> _:
+        Exception error = result.error;
+        switch (error) {
+          case Unauthorized _:
+            log("Result is Unauthorized", name: _logTag);
+          case NotFound _:
+            log("Result is NotFound", name: _logTag);
+          case RequestTimeout _:
+            log("Result is RequestTimeout", name: _logTag);
+          case ValidationError _:
+            log("Result is ValidationError: $error", name: _logTag);
+          case UnknownError _:
+            log("Result is UnknownError", name: _logTag);
+          case TokenNotFound _:
+            log("Result is TokenNotFound", name: _logTag);
+        }
+        return Result.error(error);
+    }
+  }
+
+  Future<Result<Plan>> createPlan(Plan plan) async {
+    Result<PlanApiModel> result = await _apiClient.postPlan(plan.nadeuriUuid, PlanApiModel.fromPlan(plan));
+
+    switch (result) {
+      case Ok<PlanApiModel> _:
+        log("Result is Ok: ${result.value}", name: _logTag);
+        return Result.ok(result.value.toPlan());
+      case Error<PlanApiModel> _:
         Exception error = result.error;
         switch (error) {
           case Unauthorized _:
