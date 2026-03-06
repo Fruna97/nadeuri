5be6a13a6d0aa04a77cf7f6de85664bee0391ac8
Fruna97 <kdlrlgusk@gmail.com>
2026-03-06 18:11:44 +0900

    feat(api/nadeuri): Invite 리포지토리 추가

---
+++ server/api/src/main/java/com/github/fruna97/nadeuri/domain/nadeuri/repository/InviteRepository.java
@@ -0,0 +1,8 @@
+package com.github.fruna97.nadeuri.domain.nadeuri.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.github.fruna97.nadeuri.domain.nadeuri.model.Invite;
+
+public interface InviteRepository extends JpaRepository<Invite, Long> {
+
+}
